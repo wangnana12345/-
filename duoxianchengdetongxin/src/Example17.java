f527b301d2eca84b655e3f600b17d36f3d3e11a2
wangnana12345 <55051176+wangnana12345@users.noreply.github.com>
2019-10-30 22:23:18 +0800

    多线程通信

---
+++ duoxianchengdetongxin/src/Example17.java
@@ -0,0 +1,13 @@
+
+public class Example17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Storage st=new Storage();
+		Input input=new Input(st);
+		Output output=new Output(st);
+		new Thread(input).start();
+		new Thread(output).start();
+	}
+
+}
---
+++ duoxianchengdetongxin/src/Input.java
@@ -0,0 +1,16 @@
+class Input implements Runnable{
+	private Storage st;
+	private int num;
+	Input(Storage st)
+	{
+		this.st=st;
+	}
+	public void run()
+	{
+		while(true)
+		{
+			st.put(num++);
+		}
+	}
+
+}
