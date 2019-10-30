418582826b89516f19cde6542807df7b64f6c801
wangnana12345 <55051176+wangnana12345@users.noreply.github.com>
2019-10-30 22:10:21 +0800

    实例Thread类创建多线程

---
+++ Thread01/src/Example02.java
@@ -0,0 +1,15 @@
+
+public class Example02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyThread myThread = new MyThread();
+		myThread.start();
+		while(true)
+		{
+			System.out.println("main()方法在运行");
+		}
+
+	}
+
+}
---
+++ Thread01/src/MyThread.java
@@ -0,0 +1,8 @@
+class MyThread extends Thread {
+   public void run(){
+   while(true)
+   {
+	   System.out.println("MyThread类的run()方法在运行");
+   }
+   }
+}
